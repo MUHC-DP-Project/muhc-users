01f0c740be96c849164a41ecb696859bc6812bf3
preyansh98 <preyansh98@gmail.com>
2021-03-22 21:28:01 -0400

    big thonk: add mnew auth middleware and fix env loading

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import connectDB from "./database/db_config";
 import {userRouter} from './routes/user';
 import {userAuthRouter} from './routes/userAuth';
+import nodemailer from 'nodemailer';
 
 const app = express();
 const port = 8081; // default port to listen
@@ -30,8 +31,18 @@ app.get( "/", ( req, res ) => {
 app.use('/users', userRouter);
 app.use('/auth', userAuthRouter);
 
+const emailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
 // start the Express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
 } );
+
+export {emailTransporter};
---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+async function authMiddleware(request: Request, response: Response, next: NextFunction) {
+	const authHeader = request.headers?.authorization;
+
+	if (authHeader) {
+		const token = authHeader.split(" ")[1];
+
+		try {
+            const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+			response.locals._id = decoded._id;
+
+            // fetch the user and verify if they have tokens.
+            if (decoded.isEmailVerified && decoded.isApproved) {
+                // passes auth
+                next();
+            } else {
+                throw {"error" : "user not verified or approved to access this route."};
+            }
+		} catch (jwtError) {
+			if (process.env.NODE_ENV === "dev-notoken") {
+				// special dev mode with disabled auth
+				next();
+			} else {
+                console.log("error");
+                console.log(jwtError);
+				response.sendStatus(403);
+			}
+		}
+	} else {
+        response.sendStatus(403).json({"error" : "no token provided"});
+    }
+}
+
+export default authMiddleware;
