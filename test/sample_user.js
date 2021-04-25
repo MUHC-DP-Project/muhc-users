1d4543fb2f4cd80ce9e3f0d80eaae14f1f4479de
Ege Odaci <ege.odaci@mail.mcgill.ca>
2021-04-25 15:07:34 -0400

    Add files via upload

---
+++ test/sample_user.js
@@ -0,0 +1,16 @@
+let sample_user1 = 
+    {
+     // auth
+     email: "egekarakartal@gmail.com",
+     password:"asda",
+     firstName:"Test",
+     lastName:"Test",
+     verificationNotes:"Test"
+
+
+
+    
+ 
+ };
+
+module.exports = {sample_user1};
